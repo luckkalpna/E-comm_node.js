630f89cdeba920ed323c0552a7fbc80a19a75514
luckkalpna <luckkalpna@gmail.com>
2024-10-04 21:48:53 +0530

    create signup api

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
+require("./db/config");
+const User = require("./db/User");
 const app = express();
 
-const connectDB = async () =>{
-    mongoose.connect("mongodb://localhost:27017/e-comm");
-    const productSchema = new mongoose.Schema({});
-    const product = mongoose.model("product", productSchema);
-    const data = await product.find();
-    console.log(data);
-}
-
-connectDB();
+app.use(express.json());
+app.post("/register", async (req, res)=>{
+    let user = new User(req.body)
+    let result = await user.save();
+    res.send(result)
+})
 
 app.listen(5000)
