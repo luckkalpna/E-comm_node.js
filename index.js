4482688909d438c19e65687e11b51908ad1714c0
luckkalpna <luckkalpna@gmail.com>
2024-10-05 18:56:23 +0530

    Create login API

--- index.js
+++ index.js
@@ -6,10 +6,20 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
 app.post("/register", async (req, res)=>{
     let user = new User(req.body)
     let result = await user.save();
     res.send(result)
-})
+});
 
-app.listen(5000)
+app.post("/login", async (req, res)=>{
+    let user = await User.findOne(req.body).select("-password");
+    if(user){
+        res.send(user);
+    }else{
+        res.send({result:"user not found"})
+    }
+});
+
+app.listen(5000);
