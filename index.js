1fdbc18d099b9f5c8d1df81fe017a82e6a9f613b
luckkalpna <luckkalpna@gmail.com>
2024-10-06 14:01:03 +0530

    Create Add Product API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("./db/config");
 const User = require("./db/User");
+const Product = require("./db/Product")
 const cors = require("cors");
 const app = express();
 
@@ -10,16 +11,30 @@ app.use(cors());
 app.post("/register", async (req, res)=>{
     let user = new User(req.body)
     let result = await user.save();
+    // for removing password
+    result = result.toObject();
+    delete result.password
     res.send(result)
 });
 
 app.post("/login", async (req, res)=>{
-    let user = await User.findOne(req.body).select("-password");
-    if(user){
-        res.send(user);
+    console.log(req.body)
+    if(req.body.password && req.body.email){
+        let user = await User.findOne(req.body).select("-password");
+        if(user){
+            res.send(user);
+        }else{
+            res.send({result:"user not found"})
+        }
     }else{
         res.send({result:"user not found"})
     }
 });
+
+app.post("/add-product", async (req, res) =>{
+    let product = new Product(req.body)
+    let result = await product.save();
+    res.send(result)
+})
 
 app.listen(5000);
