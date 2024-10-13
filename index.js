03fb96b25d9362d205beb06c02bdcc99e40f616a
luckkalpna <luckkalpna@gmail.com>
2024-10-13 15:51:33 +0530

    Create updateOne product API

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// API for signup any user
+
 app.post("/register", async (req, res)=>{
     let user = new User(req.body)
     let result = await user.save();
@@ -16,6 +18,8 @@ app.post("/register", async (req, res)=>{
     delete result.password
     res.send(result)
 });
+
+// API for login
 
 app.post("/login", async (req, res)=>{
     console.log(req.body)
@@ -31,11 +35,15 @@ app.post("/login", async (req, res)=>{
     }
 });
 
+// API for add new product
+
 app.post("/add-product", async (req, res) =>{
     let product = new Product(req.body)
     let result = await product.save();
     res.send(result)
 });
+
+// API for list all products
 
 app.get("/products", async (req, res)=>{
     let product = await Product.find();
@@ -46,10 +54,14 @@ app.get("/products", async (req, res)=>{
     }
 });
 
+// API for delete any product
+
 app.delete("/product/:id",async (req, res) =>{
     const result = await Product.deleteOne({_id:req.params.id})
     res.send(result);
 });
+
+// API for update page any one product
 
 app.put("/update/:_id", async (req, res) => {
     console.log(req.params)
@@ -60,6 +72,29 @@ app.put("/update/:_id", async (req, res) => {
         }
     )
     res.send(data)
+});
+
+// API for fetching any one product for update
+
+app.get("/product/:id", async (req, res) => {
+  const result = await Product.findOne({ _id: req.params.id });
+  if (result) {
+    res.send(result);
+  } else {
+    res.send({ result: "No result found" });
+  }
+});
+
+// API for update any one product
+
+app.put(("/product/:id"), async (req, res) =>{
+  let result = await Product.updateOne(
+    {_id: req.params.id},
+    {
+      $set: req.body
+    }
+  );
+  res.send(result)
 })
 
 app.listen(5000);
