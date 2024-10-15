3ffe43b3114d6110a8c7d725a898880ea78d7ca7
luckkalpna <luckkalpna@gmail.com>
2024-10-15 22:21:33 +0530

    Connect JWT token

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 require("./db/config");
 const User = require("./db/User");
-const Product = require("./db/Product")
+const Product = require("./db/Product");
+const Jwt = require("jsonwebtoken");
+const jwtKey = "e-comm";
+
 const cors = require("cors");
 const app = express();
 
@@ -16,7 +19,12 @@ app.post("/register", async (req, res)=>{
     // for removing password
     result = result.toObject();
     delete result.password
-    res.send(result)
+    Jwt.sign({result}, jwtKey, {expiresIn:"2h"}, (err, token)=>{
+      if(err){
+        res.send({result:"Somthing went wrong, please try again after some time"})
+      }
+      res.send( {result, auth:token} );
+    })
 });
 
 // API for login
@@ -26,7 +34,12 @@ app.post("/login", async (req, res)=>{
     if(req.body.password && req.body.email){
         let user = await User.findOne(req.body).select("-password");
         if(user){
-            res.send(user);
+          Jwt.sign({user}, jwtKey, {expiresIn:"2h"}, (err, token)=>{
+            if(err){
+              res.send({result:"Somthing went wrong, please try again after some time"})
+            }
+            res.send( {user, auth:token} );
+          })
         }else{
             res.send({result:"user not found"})
         }
